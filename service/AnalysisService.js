061400b941172f6de4342dbc21e2ca139ba033db
unknown <thomas.mackinson1@marist.edu>
2020-09-16 16:35:01 -0400

    Ok I was working through this all wrong. I had erros in my code which is why my tests weren't working correctly. I got it to run and mostly work. I get the words and counts into 2 seperate arrays (The corresponding count for each word has the same index. to get the count for words[3] you just call counts[3]). As before my only issue now is getting these values into a JSON object.

--- service/AnalysisService.js
+++ service/AnalysisService.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 
@@ -10,24 +11,41 @@
  **/
 exports.getConcordance = function(body) {
   return new Promise(function(resolve, reject) {
-var bodyWords = [];
-wordCount = new Map();
-bodyWords = body.split(" ")
-for(var i = 0; i < bodyWords.length; i++){
-  if(wordCount.has(bodyWords[i]) === false){
-    wordCount.set(bodyWords[i], 1);
-  } else {
-    wordCount.set(bodyWords[i], wordCount.get(bodyWords[i]) + 1);
+  var bodyWords = body.toLowerCase().split(" ");
+  var wordCount = new Map();
+  for(var i = 0; i < bodyWords.length; i++){
+      if(wordCount.has(bodyWords[i]) === false){
+        wordCount.set(bodyWords[i], 1);
+      } else {
+        wordCount.set(bodyWords[i], wordCount.get(bodyWords[i]) + 1);
+      }
   }
-}
 var words = Array.from(wordCount.keys());
 var counts = Array.from(wordCount.values());
+console.log(words);
+console.log(counts);
 var examples = {};
-examples['application/json'] = {
-  "concordance" : [ {
-      "token" : "hello",
-      "count" : 1
-  } ]
+  examples['application/json'] = {
+"input" : body,
+"concordance" : [ {
+  "token" : "brown",
+  "count" : 2
+}, {
+  "token" : "fox",
+  "count" : 1
+}, {
+  "token" : "jumped",
+  "count" : 1
+}, {
+  "token" : "log",
+  "count" : 1
+}, {
+  "token" : "over",
+  "count" : 1
+}, {
+  "token" : "the",
+  "count" : 1
+} ]
 };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
@@ -36,29 +54,3 @@ examples['application/json'] = {
     }
   });
 }
-
-    /*
-    var examples = {};
-    examples['application/json'] = {
-  "input" : "The brown fox jumped over the brown log.",
-  "concordance" : [ {
-    "token" : "brown",
-    "count" : 2
-  }, {
-    "token" : "fox",
-    "count" : 1
-  }, {
-    "token" : "jumped",
-    "count" : 1
-  }, {
-    "token" : "log",
-    "count" : 1
-  }, {
-    "token" : "over",
-    "count" : 1
-  }, {
-    "token" : "the",
-    "count" : 1
-  } ]
-};
-*/
