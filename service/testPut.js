90ada859cfc1f1439044478cefab0ea9fc6098ee
unknown <thomas.mackinson1@marist.edu>
2020-10-01 20:57:24 -0400

    simple nodejs file I copied from an aws help page, managed to manually put data, but it was successfully put into the table. Now going to try and use this test file to debug the API

---
+++ service/testPut.js
@@ -0,0 +1,35 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'us-east-2'});
+
+// Create DynamoDB document client
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var word = ["test", "this", "is", "a"];
+var count = [4, 1, 1, 1];
+
+var concordance = [];
+for(var i = 0; i < word.length; i++){
+  var concordObj = {};
+  concordObj.token = word;
+  concordObj.count = count;
+  concordance.push(concordObj);
+}
+concordance = JSON.stringify(concordance);
+
+var params = {
+  TableName: 'MSCS621-concordance-analyze',
+  Item: {
+    'input': 'TEST TEST TEST THIS IS A TEST',
+    'Concordance': concordance
+  }
+};
+
+docClient.put(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
