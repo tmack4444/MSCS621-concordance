960c5ee55cc810ac5aa940d1e451a23aea4ac206
unknown <thomas.mackinson1@marist.edu>
2020-11-10 16:48:50 -0500

    Updated my analysis controller to make a post request with AXIOS rather than call the analyze endpoint. It should POST to my url. Right now there is no security set for it, any user can POST to it, however I will set it up just for the IAM account that I created before later on

--- controllers/Analysis.js
+++ controllers/Analysis.js
@@ -4,6 +4,7 @@ var utils = require('../utils/writer.js');
 var Analysis = require('../service/AnalysisService');
 
 module.exports.getConcordance = function getConcordance (req, res, next, body) {
+  /*
   Analysis.getConcordance(body)
     .then(function (response) {
       utils.writeJson(res, response);
@@ -11,6 +12,24 @@ module.exports.getConcordance = function getConcordance (req, res, next, body) {
     .catch(function (response) {
       utils.writeJson(res, response);
     });
+  */
+  //Build a POST request in node from https://stackoverflow.com/questions/6158933/how-is-an-http-post-request-made-in-node-js
+
+  const axios = require('axios')
+
+  axios
+    .post('https://k2ici5adja.execute-api.us-east-2.amazonaws.com/default/my-function', {
+       todo: body
+     })
+    .then(res => {
+      console.log("Success");
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(res)
+    })
+    .catch(error => {
+      console.log("ERROR");
+      console.error(error)
+    })
 };
 
 module.exports.getLocations = function getLocations (req, res, next, body) {
